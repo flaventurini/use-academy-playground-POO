9acc5591b5cced76d28186c32c3a7ba1ce930c3e
Flavia Venturini <flaviaventurini@msn.com>
2022-07-30 21:23:25 -0300

    Update

--- POO-iOS.playground/Contents.swift
+++ POO-iOS.playground/Contents.swift
@@ -2,4 +2,27 @@ import Foundation
 
 //Objetos
 
+/*
 
+O struct não precisa de inicializados. O Class, o que vamos usar nesse código, precisa do 'init'
+ 
+ struct MeuCarro {
+    var portas: Int
+    var cor: String
+    var velocidadeMaxima: Int
+}
+
+let carrinho = MeuCarro(portas: <#T##Int#>, cor: <#T##String#>, velocidadeMaxima: <#T##Int#>)
+ */
+
+class Carro {
+    var portas: Int
+    var cor: String
+    var velocidadeMaxima: Int
+    
+    init(portas: Int, cor: String, velocidadeMaxima: Int) {
+        self.portas = portas
+        self.cor = cor
+        self.velocidadeMaxima = velocidadeMaxima
+    }
+}
